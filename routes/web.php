110b37e98e4f08ff5a164493feb7666946c7b68b
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-06 15:10:53 +0200

    add home view with dynamic message

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $message = 'dynamic message from route';
+    return view('home', compact('message'));
 });
